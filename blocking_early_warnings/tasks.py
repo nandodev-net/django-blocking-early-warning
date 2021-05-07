80727fb7c0abc8c4aa9d0b5e97ddfcd03365ce22
gipsy-avn <gibyvngr@gmail.com>
2021-05-07 13:37:06 -0400

    added arbitrary time interval size

--- blocking_early_warnings/tasks.py
+++ blocking_early_warnings/tasks.py
@@ -1,18 +1,21 @@
 from celery import shared_task
-import blocking_early_warnings.utils.ooni_requests  as ooni_requests
-import blocking_early_warnings.utils.list_loaders   as list_loaders
-@shared_task(time_limit = 3600, name="blocking_early_warnings.synch_metrics")
+import blocking_early_warnings.utils.ooni_requests as ooni_requests
+import blocking_early_warnings.utils.list_loaders as list_loaders
+
+
+@shared_task(time_limit=3600, name="blocking_early_warnings.synch_metrics")
 def synch_db_metrics():
     """
-        Asynch process to get raw data from ooni. 
-        Will update database so metrics object are up to date
-        with current hour and online ooni data
+    Asynch process to get raw data from ooni.
+    Will update database so metrics object are up to date
+    with current hour and online ooni data
     """
     ooni_requests.sync_db_metrics()
 
-@shared_task(time_limit = 3600, name="blocking_early_warnings.synch_urls")
+
+@shared_task(time_limit=3600, name="blocking_early_warnings.synch_urls")
 def synch_urls():
     """
-        Asynch process to get updated urls from known sources
+    Asynch process to get updated urls from known sources
     """
     list_loaders.sync_urllists_db()
