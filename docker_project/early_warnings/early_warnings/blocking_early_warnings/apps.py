ad21a542285221c485b0daab18c6b571f9322d5c
Jhon Does <jhondoes.does@gmail.com>
2023-03-30 19:52:38 -0400

    datatable construction

---
+++ docker_project/early_warnings/early_warnings/blocking_early_warnings/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BlockingEarlyWarningsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "blocking_early_warnings"
---
+++ docker_project/early_warnings/early_warnings/blocking_early_warnings/utils/__init__.py
@@ -0,0 +1,4 @@
+from .anomaly_monitor import AnomalyMonitor
+from .histogram_generator import HistogramGenerator, HistogramBlockData
+from .ooni_requests import DBMetricsClient
+from .list_loaders import ListLoader
